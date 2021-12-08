f3247c28a7ad79d6be3c53df8b7575cdf38b7c59
philwoz <61164019+philwoz@users.noreply.github.com>
2021-12-08 23:40:43 +0000

    init + created html templates

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/fixtures')
+def fixtures_page():
+    games = [
+        {'id': 1, 'home_team': 'Man City', 'away_team': 'Everton', 'H': '70%','D': '20%', 'A':'10%'},
+        {'id': 2, 'home_team': 'Chelsea', 'away_team': 'Liverpool', 'H': '65%', 'D': '25%', 'A': '10%'},
+        {'id': 3, 'home_team': 'Stoke City', 'away_team': 'Man Utd', 'H': '25%', 'D': '30%', 'A':'55%'}
+    ]
+    return render_template('fixtures.html', games=games)
