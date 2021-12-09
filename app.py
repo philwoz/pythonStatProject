25c800fca0d8f8d46da175c01e5b4325553e5584
philwoz <61164019+philwoz@users.noreply.github.com>
2021-12-09 23:41:21 +0000

    added register & login pages templates

--- app.py
+++ app.py
@@ -23,3 +23,11 @@ def table_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
